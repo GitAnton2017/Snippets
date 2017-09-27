a2d2ae22d62c5727963585057bb3bb3e61e84ce9
Anton2016 <antonk1973@MacBook-Pro.local>
2017-09-27 13:43:09 +0300

    Initial Commit

---
+++ Snippets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snippets
+//
+//  Created by Anton2016 on 27.09.17.
+//  Copyright © 2017 Anton2016. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
