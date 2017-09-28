be98555ad7dc92862d9844b4262a784ac91a3372
Anton2016 <antonk1973@MacBook-Pro.local>
2017-09-28 15:53:06 +0300

    TextSnipperEdit finished

--- Snippets/TextSnippetEntryViewController.swift
+++ Snippets/TextSnippetEntryViewController.swift
@@ -4,9 +4,25 @@ import UIKit
 class TextSnippetEntryViewController: UIViewController
 {
     
+    var textSaver : (_ text: String)->Void = {_ in }
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @objc func doneButtonPressed ()
+    {
+      textView.resignFirstResponder()
+    }
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        let keyboardToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 44))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonPressed))
+        keyboardToolbar.setItems([flexSpace, doneButton], animated: false)
+        textView.inputAccessoryView = keyboardToolbar
+        textView.becomeFirstResponder()
+        
     }
 }
     
@@ -14,8 +30,9 @@ extension TextSnippetEntryViewController : UITextViewDelegate
 {
     func textViewDidEndEditing(_ textView: UITextView)
     {
-            
+      textSaver(textView.text)
+      dismiss(animated: true, completion: nil)
     }
 }
-    
+
 
--- Snippets/ViewController.swift
+++ Snippets/ViewController.swift
@@ -18,12 +18,21 @@ class ViewController: UIViewController {
         
         let textAction = UIAlertAction(title: "TEXT", style: .default)
         {
-            (alert: UIAlertAction)->Void in self.data.append(SnippetData(type: .text))
+          _ in
+            guard let textEntryVC = self.storyboard?.instantiateViewController(withIdentifier: "textSnippetEntry") as? TextSnippetEntryViewController
+            else
+            {
+             return
+            }
+            textEntryVC.textSaver = {(text: String) in self.data.append(TextData(text: text))}
+            textEntryVC.modalTransitionStyle = .coverVertical
+            
+            self.present(textEntryVC, animated: true, completion: nil)
         }
         
         let photoAction = UIAlertAction(title: "PHOTO", style: .default)
         {
-            (alert: UIAlertAction)->Void in self.data.append(SnippetData(type: .photo))
+            _ in self.data.append(SnippetData(type: .photo))
         }
         
         let cancelAction = UIAlertAction(title: "CANCEL", style: .destructive, handler: nil)
